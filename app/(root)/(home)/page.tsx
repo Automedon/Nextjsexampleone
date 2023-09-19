7f5f6b06a4c98d924a5e1f290b045d3695882474
Automedon <42299107+Automedon@users.noreply.github.com>
2023-09-19 15:23:46 +0500

    Footer

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Page = () => {
-  return <div>page</div>;
+  return (
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full"></section>
+    </main>
+  );
 };
 
 export default Page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'Automedon Portfolio with Next.js',
-}
+  title: "Portfolio",
+  description: "Automedon Portfolio with Next.js",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className='min-h-screen bg-black-100 font-poppins'>{children}</body>
+      <body className="min-h-screen font-poppins">{children}</body>
     </html>
-  )
+  );
 }
