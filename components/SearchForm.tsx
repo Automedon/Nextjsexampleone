11e8a873372b179503103be8d914760fb8018e18
Automedon <42299107+Automedon@users.noreply.github.com>
2023-09-22 22:30:26 +0500

    SearchForm

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,10 +1,41 @@
 "use client";
-import React, { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
 import Image from "next/image";
-import { Input } from "./ui/input";
+
+import { Input } from "@/components/ui/input";
+import { formUrlQuery } from "@/sanity/utils";
 
 const SearchForm = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+
+      if (search) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: search,
+        });
+      } else {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
+
   return (
     <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
       <label className="flex-center relative w-full max-w-3xl">
@@ -13,12 +44,13 @@ const SearchForm = () => {
           className="absolute left-8"
           width={32}
           height={32}
-          alt={"Search Icon"}
+          alt="Search icon"
         />
         <Input
           className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
           type="text"
           placeholder="Search"
+          value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
       </label>
